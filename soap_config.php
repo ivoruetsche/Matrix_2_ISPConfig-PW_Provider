63b34ae96834564eab5aaff1052c27f88cced195
IvoR <20657383+ivoruetsche@users.noreply.github.com>
2024-01-03 01:50:29 +0100

    Create soap_config.php

---
+++ soap_config.php
@@ -0,0 +1,10 @@
+<?php
+# Login credentials for ISPConfig - System/Remote Users:
+#   X Mail user functions
+#   X Mail alias functions
+
+$username = 'ispconfig_matrix_gw_username';
+$password = 'ispconfig_matrix_gw_password';
+$soap_location = 'https://ispconfig.dom.tld:8080/remote/index.php';
+$soap_uri = 'https://ispconfig.dom.tld:8080/remote';
+?>
